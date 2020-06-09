60ff137ee411d763672a4dd0a851041201b65762
Ramunas-Bkr <54622805+Ramunas-Bkr@users.noreply.github.com>
2020-06-09 15:15:49 +0300

    Optimisation with HOC

--- src/index.js
+++ src/index.js
@@ -2,28 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const starWarsChars = [
-  {name: 'Luke Skywalker', side: 'light'},
-  {name: 'Yoda', side: 'light'},
-  {name: 'Obi-Wan Kenobi', side: 'light'},
-  {name: 'Palpatine', side: 'dark'},
-  {name: 'Darth Vader ', side: 'dark'},
+  { name: 'Luke Skywalker', side: 'light' },
+  { name: 'Yoda', side: 'light' },
+  { name: 'Obi-Wan Kenobi', side: 'light' },
+  { name: 'Palpatine', side: 'dark' },
+  { name: 'Darth Vader ', side: 'dark' },
 ]
-const App = ({list, side}) => {
-  
-  const filteredList =  list.filter(key => key.side === side) 
+const App = ({ list }) => (
+  <ul>
+    {list.map((char, index) => {
+      return (
+        <li key={char.name + index}>
+          <strong> {char.name} </strong> - &nbsp;
+          {char.side}
+        </li>
+      )
+    })}
+  </ul>
+)
 
-    return (
-      <ul>
-        {filteredList.map((char, index) => {
-          return (
-            <li key={char.name + index}>
-              <strong> {char.name} </strong> - &nbsp;
-              {char.side}
-            </li>
-          )
-        })}
-      </ul>
-    )
-  }
+const filteredProps = Component => ({ list, side }) => {
+  const filteredList = list.filter(key => key.side === side)
+  return (<Component list={filteredList} />)
+}
 
-ReactDOM.render(<App list={starWarsChars} side="light"/>, document.getElementById('root'));
+const FilteredList = filteredProps(App)
+
+ReactDOM
+  .render(
+    <FilteredList list={starWarsChars} side="light" />,
+    document.getElementById('root'));
