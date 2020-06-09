ca01d2da7328ec94218c64326c8f86648a06d2da
Ramunas-Bkr <54622805+Ramunas-Bkr@users.noreply.github.com>
2020-06-09 13:57:36 +0300

    Create app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 class App extends React.Component {
-  render () {
-    return (
-      <div>
+  
+  state = {
+    starWarsChars: [
+      {name: 'Luke Skywalker', side: 'light'},
+      {name: 'Yoda', side: 'light'},
+      {name: 'Obi-Wan Kenobi', side: 'light'},
+      {name: 'Palpatine', side: 'dark'},
+      {name: 'Darth Vader ', side: 'dark'},
+    ]
+  }
 
-      </div>
+render () {
+    return (
+      <ul>
+        {this.state.starWarsChars.map((char, index) => {
+          return (
+            <li key={char.name + index}>
+              <strong> {char.name} </strong> - &nbsp;
+              {char.side}
+            </li>
+          )
+        })}
+      </ul>
     )
   }
 }
